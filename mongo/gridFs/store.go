6586f1482fcafabd8c3a4b221f84868a42cdac88
José Santos <henrique_1609@me.com>
2016-03-29 19:19:16 +0100

    Add mongo plugin

---
+++ mongo/gridFs/store.go
@@ -0,0 +1,75 @@
+// mgoStore uses the mgo mongo driver to store sessions on mongo gridfs
+package gridFs
+
+import (
+	"github.com/CloudyKit/framework/context"
+	"github.com/CloudyKit/framework/session"
+	"github.com/jhsx/qm"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"io"
+	"time"
+)
+
+// New returns a new store
+func New(db, prefix string) session.Store {
+	return &Store{
+		db:     db,
+		prefix: prefix,
+	}
+}
+
+type Store struct {
+	db, prefix string
+}
+
+func (sessionStore *Store) gridFs(session *mgo.Session, name string, create bool) (*mgo.GridFile, error) {
+	session.SetMode(mgo.Strong, false)
+	gridFs := session.DB(sessionStore.db).GridFS(sessionStore.prefix)
+	if create {
+		gridFs.Remove(name)
+		gridFile, err := gridFs.Create(name)
+		return gridFile, err
+	}
+	gridFile, err := gridFs.Open(name)
+	return gridFile, err
+}
+
+func mysession(c *context.Context) (sess *mgo.Session) {
+	sess = c.Get(sess).(*mgo.Session)
+	return
+}
+
+func (sessionStore *Store) Writer(c *context.Context, name string) (writer io.WriteCloser, err error) {
+	writer, err = sessionStore.gridFs(mysession(c), name, true)
+	return
+}
+
+func (sessionStore *Store) Reader(c *context.Context, name string) (reader io.ReadCloser, err error) {
+	reader, err = sessionStore.gridFs(mysession(c), name, false)
+	return
+}
+
+func (sessionStore *Store) Remove(c *context.Context, name string) (err error) {
+	sess := mysession(c)
+	defer sess.Close()
+	return sess.DB(sessionStore.db).GridFS(sessionStore.prefix).Remove(name)
+}
+
+func (sessionStore *Store) Gc(c *context.Context, before time.Time) {
+	sess := mysession(c)
+	gridFs := sess.DB(sessionStore.db).GridFS(sessionStore.prefix)
+
+	var fileId struct {
+		Id bson.ObjectId `bson:"_id"`
+	}
+
+	inter := gridFs.Find(qm.Lt("uploadDate", before)).Iter()
+
+	for inter.Next(&fileId) {
+		gridFs.RemoveId(fileId.Id)
+	}
+
+	inter.Close()
+	sess.Close()
+}
